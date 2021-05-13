e8c30391e99975eba8e861a8f6c194aa2987e6d5
ark4579 <ark4579@hotmail.pk>
2021-05-13 16:00:19 +0500

    initial commit

---
+++ example/dj_fc_example.dart
@@ -0,0 +1,6 @@
+import 'package:dj_fc/dj_fc.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dj_fc.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dj_fc;
+
+export 'src/dj_fc_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
